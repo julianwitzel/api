f9ede5480d1c82f1610711dc19e63be99844c947
julianwitzel <julian@vierless.de>
2024-09-03 22:09:06 +0200

    Updated /api/index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
 const airtableRoute = require('./routes/airtable');
 const errorRoute = require('./routes/error');
 
 const app = express();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 // Airtable route
 app.use('/api/airtable', airtableRoute);
@@ -11,14 +16,20 @@ app.use('/api/airtable', airtableRoute);
 app.use('/api/error', errorRoute);
 
 // Root route
-app.get('/', (req, res) => {
-	res.send('Welcome to the Vierless API');
+app.get('/', async (req, res) => {
+	try {
+		const landingPath = path.join(process.cwd(), 'public', 'html', 'index.html');
+		const content = await fs.readFile(landingPath, 'utf8');
+		res.send(content);
+	} catch (err) {
+		console.error('Error reading landing page:', err);
+		res.status(500).send('An error occurred while loading the landing page');
+	}
 });
 
 // 404 handler
 app.use((req, res, next) => {
 	res.status(404);
-	// Pass the error to the error handler
 	next(new Error('Not Found'));
 });
 
@@ -26,9 +37,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
 	const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
 	res.status(statusCode);
-
-	// Instead of redirecting, we'll render the error page directly
-	errorRoute(req, res, next);
+	errorRoute.renderErrorPage(req, res, statusCode);
 });
 
 module.exports = app;
