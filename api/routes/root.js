6f856fb9f489aa32c3a2bdff17fc3ab1663f5ff4
julianwitzel <julian@vierless.de>
2024-09-03 20:29:39 +0200

    Added index.html for project

---
+++ api/routes/root.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+module.exports = async (req, res) => {
+	try {
+		const filePath = path.join(process.cwd(), 'public', 'landing.html');
+		const content = await fs.readFile(filePath, 'utf8');
+		res.setHeader('Content-Type', 'text/html');
+		res.status(200).send(content);
+	} catch (error) {
+		console.error('Error serving landing page:', error);
+		res.status(500).send('An error occurred while loading the page');
+	}
+};
